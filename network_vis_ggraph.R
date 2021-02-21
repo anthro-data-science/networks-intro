9599b7e64a0b0913c5d3ba07ba379f56b0ad2622
Liying Wang <jaybin502@gmail.com>
2021-02-21 12:30:18 -0800

    update code for visualization

--- network_vis_ggraph.R
+++ network_vis_ggraph.R
@@ -3,28 +3,73 @@
 # load packages
 library(statnet)
 library(tidygraph)
-library(ggraph)
+library(ggraph) # ggplot2 of networks
 
 # network visualization
-# Example: Karate club social network of Zachary (1977)
 data(zach)
-net_statnet <- network(zach)
 net_ggraph <- intergraph::asIgraph(zach)
 
-ggraph(net_ggraph, 
-       layout = "fr") + 
-    geom_edge_link(aes(color = factor(contexts))) + 
-    geom_node_point(aes(color = role)) 
+# get degree
+graph_tbl <- net_ggraph %>% 
+    as_tbl_graph() %>% 
+    activate(nodes) %>% 
+    mutate(degree = centrality_degree())
 
-# different layout
-# an arc diagram
-ggraph(net_ggraph, 
-       layout = "linear") +
-    geom_edge_arc(aes(colour = factor(contexts)))
+#net_ggraph <- create_layout(graph_tbl, 
+                        #layout = 'igraph', 
+                        #algorithm = 'nicely')
 
-# a coord diagram
-ggraph(net_ggraph, 
-       layout = "linear", 
+# basic visualization
+ggraph(graph_tbl, 
+       layout = "stress") + 
+    geom_edge_link() +
+    geom_node_point() +
+    theme_graph()
+
+# aesthetics for edges
+ggraph(graph_tbl, 
+       layout = "stress") + 
+    geom_edge_link(aes(width = contexts),
+                       alpha = 0.6) +
+    scale_edge_width_continuous(range = c(0.2,1.5)) +
+    geom_node_point() +
+    theme_graph()
+
+# aesthetics for nodes
+ggraph(graph_tbl, 
+       layout = "stress") + 
+    geom_edge_link(aes(width = contexts),
+                   alpha = 0.6) +
+    scale_edge_width_continuous(range = c(0.2,1.5)) +
+    geom_node_point(aes(size = degree,
+                        color = role)) + 
+    geom_node_text(aes(filter = degree >= 12, 
+                       label = club)) +
+    theme_graph()
+
+# facet nodes by node attributes
+ggraph(graph_tbl) + 
+    geom_edge_link(aes(width = contexts),
+                   alpha = 0.6) + 
+    scale_edge_width_continuous(range = c(0.2,1.5)) +
+    geom_node_point(aes(size = degree,
+                        color = role)) +
+    facet_nodes(~club) +
+    theme_graph()
+
+# different layout- circle
+ggraph(graph_tbl, 
+       layout = "linear",
        circular = TRUE) + 
-    geom_edge_arc(aes(colour = factor(contexts)))
+    geom_edge_link(alpha = 0.5) +
+    geom_node_point(aes(size = degree,
+                        color = role)) + 
+    theme_graph()
 
+# different layout- star
+ggraph(graph_tbl, 
+       layout = "star") + # grid, dh, gem, etc.
+    geom_edge_link(alpha = 0.5) +
+    geom_node_point(aes(size = degree,
+                        color = role)) + 
+    theme_graph()
